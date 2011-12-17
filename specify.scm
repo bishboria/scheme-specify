5aabe8531a4df7704c9cef0acbc452cf6170b726
Stuart Gale <bishboria@gmail.com>
2011-12-17 01:36:34 +0000

    Loading the test framework file in the test file.

--- specify.scm
+++ specify.scm
@@ -1,22 +1,4 @@
-(define (specify . facts)
-  (let ((correct (filter correct? facts))
-	(incorrect (filter incorrect? facts)))
-    (append correct incorrect)))
-
-(define (correct? fact)
-  (if (equal? fact '.)
-    #t
-    #f))
-
-(define (incorrect? fact)
-  (not (correct? fact)))
-
-(define-syntax fact 
-  (syntax-rules ()
-		((delay e a)
-		 (if (equal? e a)
-		   '.
-		   (list 'expected 'a 'to 'be e 'but 'is a)))))
+(load "framework/specify.scm")
 
 (specify
   (fact #f #t)
