382226604ef9a41a03cc509171cd4a7f642d2fe0
Stuart Gale <bishboria@gmail.com>
2011-12-17 00:41:21 +0000

    Tests now show correct first then all the failures.
    Driven by tests, too. Weird.

--- specify.scm
+++ specify.scm
@@ -1,12 +1,22 @@
 (define (specify . facts)
-  facts)
+  (let ((correct (filter correct? facts))
+	(incorrect (filter incorrect? facts)))
+    (append correct incorrect)))
+
+(define (correct? fact)
+  (if (equal? fact '.)
+    #t
+    #f))
+
+(define (incorrect? fact)
+  (not (correct? fact)))
 
 (define-syntax fact 
   (syntax-rules ()
-    ((delay e a)
-       (if (equal? e a)
-	 '.
-	 (list 'expected 'a 'to 'be e 'but 'is a)))))
+		((delay e a)
+		 (if (equal? e a)
+		   '.
+		   (list 'expected 'a 'to 'be e 'but 'is a)))))
 
 (specify
   (fact #f #t)
@@ -14,3 +24,16 @@
   (fact #t (< 2 1))
   (fact 3 (+ 2 2))
   (fact 2 (sqrt 4)))
+
+(specify
+  (fact (list '. '(expected #t to be #f but is #t))
+	(specify
+	  (fact #t (= 1 1))
+	  (fact #f #t)))
+  (fact (list '. '. '(expected #t to be #f but is #t))
+	(specify
+	  (fact #t (= 1 1))
+	  (fact #f #t)
+	  (fact #t #t)))
+  (fact #t (correct? '.))
+  (fact #f (correct? '(expected #t to be #f but is #t))))
