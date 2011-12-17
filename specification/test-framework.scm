975b90b370701adef36e3a067344854a9e323520
Stuart Gale <bishboria@gmail.com>
2011-12-17 02:01:30 +0000

    Updating initial examples. Restoring proper order of modification tests used for driving.

--- specification/test-framework.scm
+++ specification/test-framework.scm
@@ -3,10 +3,10 @@
 (specify
   (fact #t #t)
   (fact #t (= 1 1))
-  (fact #t (< 1 2))
-  (fact #t (> 2 1))
   (fact 3 (+ 1 2))
+  (fact #t (< 1 2))
   (fact #t (< 2 1))
+  (fact #t (> 2 1))
   (fact 3 (sqrt 4)))
 
 (specify
@@ -16,8 +16,8 @@
 	  (fact #t (= 1 2))))
   (fact (list '. '. '(expected (= 1 2) to be #t but is #f))
 	(specify
+	  (fact #t (= 1 2))
 	  (fact #t #t)
-	  (fact #t (= 1 1))
-	  (fact #t (= 1 2))))
+	  (fact #t (= 1 1))))
   (fact #t (correct? '.))
   (fact #f (correct? '(expected (= 1 2) to be #t but is #f))))
