91ef18bda059f28eb310bbd170f58654bd8f1e3c
Stuart Gale <bishboria@gmail.com>
2011-12-17 01:48:28 +0000

    Updating examples to make them clearer.

--- specify.scm
+++ specify.scm
@@ -1,21 +1,23 @@
 (load "framework/specify.scm")
 
 (specify
-  (fact #f #t)
+  (fact #t #t)
+  (fact #t (= 1 1))
   (fact #t (< 1 2))
+  (fact #t (> 2 1))
+  (fact 3 (+ 1 2))
   (fact #t (< 2 1))
-  (fact 3 (+ 2 2))
-  (fact 2 (sqrt 4)))
+  (fact 3 (sqrt 4)))
 
 (specify
-  (fact (list '. '(expected #t to be #f but is #t))
+  (fact (list '. '(expected (= 1 2) to be #t but is #f))
 	(specify
 	  (fact #t (= 1 1))
-	  (fact #f #t)))
-  (fact (list '. '. '(expected #t to be #f but is #t))
+	  (fact #t (= 1 2))))
+  (fact (list '. '. '(expected (= 1 2) to be #t but is #f))
 	(specify
+	  (fact #t #t)
 	  (fact #t (= 1 1))
-	  (fact #f #t)
-	  (fact #t #t)))
+	  (fact #t (= 1 2))))
   (fact #t (correct? '.))
-  (fact #f (correct? '(expected #t to be #f but is #t))))
+  (fact #f (correct? '(expected (= 1 2) to be #t but is #f))))
